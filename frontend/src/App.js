c7cb785d3f5b17105930cf750f4af9de42d9f035
srsandy <sandeepranjan2007@yahoo.co.in>
2019-03-26 21:10:57 +0530

    frontend: App started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Post from './components/Post';
 
 class App extends Component {
   render() {
     return (
        <div>
        		<h1>Nested-Comments</h1>
+       		<Post></Post>
        </div>
     );
   }
